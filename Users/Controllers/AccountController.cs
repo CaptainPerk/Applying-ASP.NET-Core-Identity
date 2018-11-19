98b42e33cd73e8377a51aaf8afcdce68594b3d8c
Jon Perkins <captainperk81@gmail.com>
2018-11-19 06:55:07 -0500

    Implementing User Authentication

---
+++ Users/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel login, string returnUrl)
+        {
+            return View(login);
+        }
+    }
+}
